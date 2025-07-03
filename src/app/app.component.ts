62346896a8a480b7b11b7e9c2136458ba54179bd
yasvizeu <yasminvizeusanttana@yahoo.com>
2025-07-03 14:46:46 -0300

    cards components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../shared/carousel/carousel.component';
+import { CardsComponent } from "../../shared/cards/cards.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, CardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
